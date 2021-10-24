cda43bd45350fff2e4ccc2d50b519c605e03fe25
Alfan Herya <alfanherya@gmail.com>
2021-10-24 07:20:33 +0700

    master code untuk belajar

---
+++ belajar-spring-dasar/src/test/java/herya/programmer/belajarspringdasar/BelajarSpringDasarApplicationTests.java
@@ -0,0 +1,13 @@
+package herya.programmer.belajarspringdasar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringDasarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
