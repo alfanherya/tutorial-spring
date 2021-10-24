ef610675c4ffe59e56bbabae22afbf3c699e7912
Alfan Herya <alfanherya@gmail.com>
2021-10-24 07:55:14 +0700

    pelajaran kedua

---
+++ belajar-spring-dasar/src/main/java/herya/programmer/belajarspringdasar/Database.java
@@ -0,0 +1,18 @@
+package herya.programmer.belajarspringdasar;
+
+import javax.xml.crypto.Data;
+
+public class Database {
+    private static Database database;
+
+    public static Database getInstance(){
+        if (database == null){
+            database = new Database();
+        }
+        return  database;
+    }
+
+    private Database(){
+
+    }
+}
