35956211a856ac99aa4d077bf67abe85efaf05f6
Alfan Herya <alfanherya@gmail.com>
2021-10-24 07:34:36 +0700

    pelajaran pertama

---
+++ belajar-spring-dasar/src/main/java/herya/programmer/belajarspringdasar/HelloWorldConfiguration.java
@@ -0,0 +1,7 @@
+package herya.programmer.belajarspringdasar;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloWorldConfiguration {
+}
---
+++ belajar-spring-dasar/src/test/java/herya/programmer/belajarspringdasar/ApplicationContextTest.java
@@ -0,0 +1,15 @@
+package herya.programmer.belajarspringdasar;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextTest {
+    @Test
+    void testApplicationContext(){
+        ApplicationContext contextTest = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+
+        Assertions.assertNotNull(contextTest);
+    }
+}
