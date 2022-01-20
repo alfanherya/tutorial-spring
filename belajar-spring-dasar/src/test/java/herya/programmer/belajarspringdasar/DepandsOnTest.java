f9942f226804b1763c767827891ca1972f6d1ec5
adl.alfanh@xl.co.id <adl.alfanh@xl.co.id>
2022-01-20 22:26:44 +0700

    lazy bean(annotation untuk menjalankan bean ketika dibutuhkan, jika tidak bean akan tetap off

--- belajar-spring-dasar/src/test/java/herya/programmer/belajarspringdasar/DepandsOnTest.java
+++ belajar-spring-dasar/src/test/java/herya/programmer/belajarspringdasar/DepandsOnTest.java
@@ -1,5 +1,6 @@
 package herya.programmer.belajarspringdasar;
 
+import herya.programmer.belajarspringdasar.data.Foo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
@@ -16,6 +17,6 @@ public class DepandsOnTest {
 
     @Test
     void testDependsOn(){
-
+        Foo foo = applicationContext.getBean(Foo.class);
     }
 }
